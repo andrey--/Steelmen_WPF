41547f8d7bd7ffbc8f541c55b2497a761b11cd36
Andrii Kovalenko <akovalenko@lohika.com>
2019-01-12 18:28:07 +0200

    Add project files.

---
+++ Steelmen/Guy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Steelmen
+{
+    class Guy
+    {
+        public string Name;
+        public int Cash;
+
+        public int GiveCash(int amount)
+        {
+
+        }
+    }
+}
